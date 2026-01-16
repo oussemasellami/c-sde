2a865b1634bf6f8069a6dd56664cbe2fd7123c84
oussema.sellami <49963438+oussemasellami@users.noreply.github.com>
2026-01-16 20:20:23 +0100

    mongorep

--- src/users/user/user.controller.ts
+++ src/users/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.model';
 import { ObjectId } from 'typeorm';
@@ -8,46 +19,67 @@ import { FilterbyroleInterceptor } from 'src/filterbyrole/filterbyrole.intercept
 //@UseInterceptors(FilterbyroleInterceptor)
 @Controller('user')
 export class UserController {
-constructor(private readonly userService:UserService){}
+  constructor(private readonly userService: UserService) {}
 
-@Post("add")
-createuser(@Body() data:createuserdto){
-    return this.userService.create(data)
-}
+  @Post('add')
+  createuser(@Body() data: createuserdto) {
+    return this.userService.create(data);
+  }
 
-@Get("showuser")
-getalluser(){
-    return this.userService.getallusers()
-   
-}
+  @Get('showuser')
+  getalluser() {
+    return this.userService.getallusers();
+  }
 
-@Get("getdatesixMonth")
-getdatesixMonth(){
+  @Get('getdatesixMonth')
+  getdatesixMonth() {
     //return this.userService.getsixmois()
-     return this.userService.getdays()
-   
-}
+    return this.userService.getdays();
+  }
 
+  @Get('showuserbyrole/:role')
+  getbyrole(@Param('role') role: string) {
+    return this.userService.getbyrole(role);
+  }
 
-@Get("showuserbyrole/:role")
-getbyrole(@Param("role") role:string){
-    return this.userService.getbyrole(role)
-}
+  @Get('showuser/:id')
+  getoneuser(@Param('id') id: ObjectId) {
+    return this.userService.getoneuser(id);
+  }
 
-@Get("showuser/:id")
-getoneuser(@Param('id') id:ObjectId){
-    return this.userService.getoneuser(id)
-}
+  @Put('updateuser/:id')
+  updateuser(@Param('id') id: ObjectId, @Body() data: User) {
+    return this.userService.updateuser(id, data);
+  }
 
-@Put("updateuser/:id")
-updateuser(@Param('id')id:ObjectId,@Body() data:User){
-    return this.userService.updateuser(id,data)
-}
+  @Delete('deleteuser/:id')
+  deleteuser(@Param('id') id: ObjectId) {
+    return this.userService.deleteuser(id);
+  }
 
-@Delete("deleteuser/:id")
-deleteuser(@Param('id')id:ObjectId){
-    return this.userService.deleteuser(id)
-}
+  //ghofrane oualhazi:+5
+  @Get('showpaginator/:page/:limit')
+  getpaginator(
+    @Param('page', ParseIntPipe) page: any,
+    @Param('limit', ParseIntPipe) limit: any,
+  ) {
+    return this.userService.getPaginatorsUsers(page, limit);
+  }
 
+  @Get('getTri')
+  gettri() {
+    //return this.userService.getsixmois()
+    return this.userService.gettriUsers();
+  }
 
+  @Get('getdesact')
+  getdesact() {
+    //return this.userService.getsixmois()
+    return this.userService.Desactivate();
+  }
+
+  @Get('updateRole/:domain/:myrole')
+  updateRole(@Param('domain') domain: any, @Param('myrole') myrole: any) {
+    return this.userService.updateRole(domain, myrole);
+  }
 }
--- src/users/user/user.service.ts
+++ src/users/user/user.service.ts
@@ -6,131 +6,182 @@ import { createuserdto } from './creteuser.dto';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: MongoRepository<User>,
+  ) {}
 
-    constructor(@InjectRepository(User) private readonly userRepository:MongoRepository<User>){}
+  async create(data: createuserdto): Promise<string> {
+    try {
+      await this.userRepository.save(data);
 
-    async create(data:createuserdto):Promise<string>{
-        try{
-           
-             await this.userRepository.save(data)
-
-         return 'user created'
-            
-        }catch(err){
-            return 'err user not created'
-            //console.log(err)
-        }
-        
+      return 'user created';
+    } catch (err) {
+      return 'err user not created';
+      //console.log(err)
     }
+  }
 
-    async getallusers():Promise<User[] >{
-           try{
-            const user=await this.userRepository.find()
-             return user
-        }catch(err){
-return []
-        }
-        
+  async getallusers(): Promise<User[]> {
+    try {
+      const user = await this.userRepository.find();
+      return user;
+    } catch (err) {
+      return [];
     }
+  }
 
-    async getoneuser(id:ObjectId):Promise<User|null>{
-           try{
-                 const user=await this.userRepository.findOneBy(id)
-                  return user
-        }catch(err){
-            return null
- //console.log(err)
-        }
-   
+  async getoneuser(id: ObjectId): Promise<User | null> {
+    try {
+      const user = await this.userRepository.findOneBy(id);
+      return user;
+    } catch (err) {
+      return null;
+      //console.log(err)
     }
-    async updateuser(id:ObjectId,data:User):Promise<string>{
-           try{
-              await this.userRepository.update(id,data)
-              return 'user updated'
-        }catch(err){
-return 'err'
- //console.log(err)
-        }
-       
+  }
+  async updateuser(id: ObjectId, data: User): Promise<string> {
+    try {
+      await this.userRepository.update(id, data);
+      return 'user updated';
+    } catch (err) {
+      return 'err';
+      //console.log(err)
     }
+  }
 
-      async deleteuser(id:ObjectId):Promise<string>{
-           try{
-             await this.userRepository.delete(id)
-              return 'user deleted'
-        }catch(err){
-             return 'err'
- console.log(err)
-        }
-       
+  async deleteuser(id: ObjectId): Promise<string> {
+    try {
+      await this.userRepository.delete(id);
+      return 'user deleted';
+    } catch (err) {
+      return 'err';
+      console.log(err);
     }
+  }
 
-
-      async getbyrole(role:string):Promise<User[] >{
-           try{
-            const user=await this.userRepository.find(
-                {select:
-                    ["username","role"]
-                     ,where:{"role":role}      
-                }
-            )
-             return user
-        }catch(err){
-return []
-        }
-        
+  async getbyrole(role: string): Promise<User[]> {
+    try {
+      const user = await this.userRepository.find({
+        select: ['username', 'role'],
+        where: { role: role },
+      });
+      return user;
+    } catch (err) {
+      return [];
     }
+  }
 
-
-          async getbydomain(domain:string):Promise<User[] >{
-           try{
-            const user=await this.userRepository.find({
-               
-                     where:{
-                        email:{
-                        $regex:`@${domain}$`
-                     }}      
-                }
-            )
-             return user
-        }catch(err){
-return []
-        }
-        
+  async getbydomain(domain: string): Promise<User[]> {
+    try {
+      const user = await this.userRepository.find({
+        where: {
+          email: {
+            $regex: `@${domain}$`,
+          },
+        },
+      });
+      return user;
+    } catch (err) {
+      return [];
     }
+  }
 
-
-    
-          async getsixmois():Promise<User[] >{
-           try{
-            const sixmois=new Date()
-            sixmois.setMonth(sixmois.getMonth()-6)
-            const user=await this.userRepository.find(
-                {updatedAt:{$exists:true} ,  $expr:
-                        {$lt:[{$toDate:"$updatedAt"},sixmois]}     
-                }
-
-            )
-             return user
-        }catch(err){
-return []
-        }  
+  async getsixmois(): Promise<User[]> {
+    try {
+      const sixmois = new Date();
+      sixmois.setMonth(sixmois.getMonth() - 6);
+      const user = await this.userRepository.find({
+        updatedAt: { $exists: true },
+        $expr: { $lt: [{ $toDate: '$updatedAt' }, sixmois] },
+      });
+      return user;
+    } catch (err) {
+      return [];
     }
+  }
 
-
-              async getdays():Promise<User[] >{
-           try{
-            const days=new Date()
-            days.setDate(days.getDate()-7)
-            const user=await this.userRepository.find(
-                {updatedAt:{$exists:true} ,  $expr:
-                        {$gt:[{$toDate:"$updatedAt"},days]}     
-                }
-
-            )
-             return user
-        }catch(err){
-return []
-        }  
+  async getdays(): Promise<User[]> {
+    try {
+      const days = new Date();
+      days.setDate(days.getDate() - 7);
+      const user = await this.userRepository.find({
+        updatedAt: { $exists: true },
+        $expr: { $gt: [{ $toDate: '$updatedAt' }, days] },
+      });
+      return user;
+    } catch (err) {
+      return [];
     }
+  }
+
+  async getPaginatorsUsers(page: number, limit: number) {
+    try {
+      console.log(page, limit);
+      const user = await this.userRepository.find({
+        skip: (page - 1) * limit,
+        take: limit,
+      });
+      return user;
+    } catch (err) {
+      return [];
+    }
+  }
+
+  async gettriUsers() {
+    try {
+      const user = await this.userRepository.find({
+        order: {
+          updatedAt: 'DESC',
+        },
+      });
+      return user;
+    } catch (err) {
+      return [];
+    }
+  }
+
+  async gettriMulti() {
+    try {
+      const user = await this.userRepository.find({
+        order: {
+          role: 'ASC',
+          updatedAt: 'DESC',
+        },
+      });
+      return user;
+    } catch (err) {
+      return [];
+    }
+  }
+
+  async Desactivate() {
+    try {
+      const sixmois = new Date();
+      sixmois.setMonth(sixmois.getMonth() - 6);
+      const user = await this.userRepository.updateMany(
+        {
+          $expr: { $lte: [{ $toDate: '$updatedAt' }, sixmois] },
+        },
+        { $set: { status: 'not done' } },
+      );
+      return user;
+    } catch (err) {
+      return [];
+    }
+  }
+
+  async updateRole(domain: string, myrole: string) {
+    try {
+      const user = await this.userRepository.updateMany(
+        {
+          email: { $regex: `@${domain}$` },
+        },
+        { $set: { role: myrole } },
+      );
+      return user;
+    } catch (err) {
+      return [];
+    }
+  }
 }
